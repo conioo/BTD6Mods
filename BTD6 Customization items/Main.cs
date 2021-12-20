0840ba1345c9caff63b5183e73d9482d5a629c62
Adek Conio <81613391+GMConio@users.noreply.github.com>
2021-12-20 21:44:47 +0100

    Add files via upload

--- BTD6 Customization items/Main.cs
+++ BTD6 Customization items/Main.cs
@@ -5,8 +5,8 @@ namespace BTD6_Customization_items
 {
     class Main : BloonsTD6Mod
     {
-        public override string MelonInfoCsURL => "https://raw.githubusercontent.com/doombubbles/BTD6-Mods/main/AutoEscape/Main.cs";
-        public override string LatestURL => "https://github.com/doombubbles/BTD6-Mods/blob/main/AutoEscape/AutoEscape.dll?raw=true";
+        public override string MelonInfoCsURL => "https://raw.githubusercontent.com/GMConio/BTD6Mods/main/BTD6%20Customization%20items/Properties/AssemblyInfo.cs?token=ATOVET2CBOHZTBXP3AIIW6DBYDVHW";
+        public override string LatestURL => "https://github.com/GMConio/BTD6Mods/blob/main/BTD6%20Customization%20items/BTD6%20Customization%20items.dll?raw=true";
 
         public override void OnApplicationStart()
         {
