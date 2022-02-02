ed85f723f565dcc6e0aff0aac003fbae387ff0a1
Adek Conio <poscerbik@gmail.com>
2022-02-02 11:30:40 +0100

    update

--- BTD6RandomMonkeysGenerator/Main.cs
+++ BTD6RandomMonkeysGenerator/Main.cs
@@ -20,9 +20,9 @@ namespace BTD6RandomMonkeysGenerator
 {
     class Main : BloonsTD6Mod
     {
-        // public override string MelonInfoCsURL => "https://raw.githubusercontent.com/GMConio/BTD6Mods/main/BTD6RandomMonkeysGenerator/Properties/AssemblyInfo.cs";
+        public override string MelonInfoCsURL => "https://raw.githubusercontent.com/GMConio/BTD6Mods/main/BTD6RandomMonkeysGenerator/Properties/AssemblyInfo.cs";
 
-        // public override string LatestURL => "https://github.com/GMConio/BTD6Mods/blob/main/BTD6RandomMonkeysGenerator/BTD6RandomMonkeysGenerator.dll?raw=true";
+        public override string LatestURL => "https://github.com/GMConio/BTD6Mods/blob/main/BTD6RandomMonkeysGenerator/BTD6RandomMonkeysGenerator.dll?raw=true";
 
         public override void OnApplicationStart()
         {
--- BTD6RandomMonkeysGenerator/Properties/AssemblyInfo.cs
+++ BTD6RandomMonkeysGenerator/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
-// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
-// powiązane z zestawem.
 [assembly: AssemblyTitle("Szablon_BTD6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -18,24 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: MelonInfo(typeof(BTD6RandomMonkeysGenerator.Main), "BTD6RandomMonkeysGenerator", "1.0.0", "Adek")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
-
-// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
-// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
-// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
 [assembly: ComVisible(false)]
 
-// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
 [assembly: Guid("322c0fc6-070b-49ed-b1a6-91af59e18fb8")]
 
-// Informacje o wersji zestawu zawierają następujące cztery wartości:
-//
-//      Wersja główna
-//      Wersja pomocnicza
-//      Numer kompilacji
-//      Poprawka
-//
-// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
-// przy użyciu symbolu „*”, tak jak pokazano poniżej:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
