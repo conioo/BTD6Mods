a161e9914b7fad0157e716e52bd5cb8bc61e1568
GMConio <poscerbik@gmail.com>
2022-09-18 12:10:44 +0200

    fixed icon safemonkey

--- BTD6SafeMonkey/ModHelperData.cs
+++ BTD6SafeMonkey/ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string Version = "1.3.1";
+        public const string Version = "1.3.2";
         public const string Name = "SafeMonkey";
 
         public const string Description = "Adds new monkey SafeMonkey";
@@ -10,7 +10,7 @@
         public const string RepoOwner = "conioo";
         public const string RepoName = "BTD6Mods";
 
-        public const string Icon = "Image\\Icon\\SafeIcon.png";
+        public const string Icon = "Image/Icon/SafeIcon.png";
 
         public const string SubPath = "main/BTD6SafeMonkey";
         public const string DllName = "BTD6SafeMonkey.dll";
